4ab849e6f44b0a039afec8facc527e441a5e3fb6
kabir yadav <kabiryadav0101@gmail.com>
2025-03-10 23:10:42 +0530

    Changig name

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,9 +17,9 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to GupShup !</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a Account from the sidebar to start chatting
         </p>
       </div>
     </div>
